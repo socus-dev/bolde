e6b56f581be77865cd5b93f8e4d1a291e3db8072
socus-dev <beheer@socus.nl>
2021-08-13 13:52:40 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Branding, design and development studio." />
         <p className="description">
         Branding, design and development studio. <code>pages/index.js</code>
         </p>
